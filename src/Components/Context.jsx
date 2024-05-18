080eb4cf156170f98517f544709f0a35b9dda1f7
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-05-18 14:45:20 -0600

    Create main.jsx and context.jsx

---
+++ src/Components/Context.jsx
@@ -0,0 +1,9 @@
+
+
+const Context = ({children}) => {
+  return (
+    <div className="context" >{children}</div>
+  )
+}
+
+export default Context
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div>
+        <h1>Hello, world!</h1>
+        <h2>Welcome to React</h2>
+    </div>
   </React.StrictMode>,
 )
