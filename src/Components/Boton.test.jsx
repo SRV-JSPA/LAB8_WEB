24a2fc68f795d51001af76939da37f750dac0141
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-05-25 21:59:14 -0600

    Cambios en boton.test.jsx

--- src/Components/Boton.test.jsx
+++ src/Components/Boton.test.jsx
@@ -1,7 +1,35 @@
-import { describe, test, expect } from "vitest";
+import { describe, it, beforeEach, vi, expect } from 'vitest';
+import { render, fireEvent } from '@testing-library/react';
+import Boton from './Boton';
+import { CalcuContext } from '../Context/CalcuContext';
 
-describe('Accordion', () => {
-    test('should add two numbers', () => {
-        expect(1 + 1).toBe(2)
-    })
-})
+describe('Boton Component', () => {
+  let mockSetCalcu;
+
+  beforeEach(() => {
+    mockSetCalcu = vi.fn(); 
+  });
+
+  it('should handle click event correctly', () => {
+    const value = {
+      calcu: { numero: 5, signo: '', resultado: 0 },
+      setCalcu: mockSetCalcu,
+      valor: '5'
+    };
+
+    const { getByText } = render(
+      <CalcuContext.Provider value={value}>
+        <Boton valor={value.valor} />
+      </CalcuContext.Provider>
+    );
+
+    const button = getByText('5');
+    fireEvent.click(button);
+
+    expect(mockSetCalcu).toHaveBeenCalledWith({
+      numero: 55, 
+      signo: '',
+      resultado: 0
+    });
+  });
+});
